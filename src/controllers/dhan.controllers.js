4f013be75db63a6852e04721116bd4344003dae0
UdayShankar754 <uday.naxtre@gmail.com>
2024-07-31 10:28:04 +0530

    added data of 31st july

--- src/controllers/dhan.controllers.js
+++ src/controllers/dhan.controllers.js
@@ -4,6 +4,21 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import { Dhan } from "../models/dhan.models.js"
 import axios from "axios"
+import fs from 'fs';
+import path from 'path';
+import XLSX from 'xlsx'; // Import XLSX library
+
+// Function to get the current date and time in the format ddmmyyyyhhmm
+const getFormattedDateTime = () => {
+  const now = new Date();
+  const day = String(now.getDate()).padStart(2, '0');
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+  const year = now.getFullYear();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+
+  return `${day}${month}${year}${hours}${minutes}`;
+};
 
 const uploadDhanDataToDb = asyncHandler(async (req, res) => {
   let url = 'https://ow-scanx-analytics.dhan.co/customscan/fetchdt'
@@ -110,7 +125,25 @@ const uploadDhanDataToDb = asyncHandler(async (req, res) => {
     const allResponse = await axios.post(url,reqBody)
     if(allResponse?.data?.data?.length > 0) {
       // const avilableInDb  = await Dhan
-      finalResponse = await Dhan.insertMany(allResponse?.data?.data)
+      finalResponse = await Dhan.insertMany(allResponse?.data?.data);
+      const xlsxName = Dhan.modelName;
+      const dateTimeString = getFormattedDateTime();
+      const directoryPath = path.join('Dhan', dateTimeString);
+      const filePath = path.join(directoryPath, `${xlsxName}.xlsx`);
+
+      if (!fs.existsSync(directoryPath)) {
+        fs.mkdirSync(directoryPath, { recursive: true });
+      }
+
+      // Create a new workbook and add the worksheet
+      const workbook = XLSX.utils.book_new();
+      const worksheet = XLSX.utils.json_to_sheet(allResponse?.data?.data);
+      XLSX.utils.book_append_sheet(workbook, worksheet, 'Top Gainers');
+
+      // Write to file
+      XLSX.writeFile(workbook, filePath);
+
+      console.log(`Excel file created successfully at ${filePath}`);
       return res.status(200).json(
         new ApiResponse(200, "Data Updated In DB Successfully" , `${allResponse?.data?.data?.length} records Inserted`)
       )
--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDatabase = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME_2}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME_3}`);
         console.log(` \n Mongoose Connect !! ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("Mongoose Connection Error : ",error.message);
